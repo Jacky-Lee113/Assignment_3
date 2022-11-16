ac38915c710b086efcd5efa1b7168871d872fdc2
Rui Felipe David <ruifelipe@hotmail.com>
2022-11-16 16:42:45 -0500

    product model

--- Code/cash_register/models.py
+++ Code/cash_register/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.TextField(max_length=200)
+    code = models.IntegerField(default=0)
+    price = models.FloatField(default=0)
