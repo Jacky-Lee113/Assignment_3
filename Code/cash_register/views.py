c0dbee220d2cb37cca9a155c69c531ee96e06605
Rui Felipe David <ruifelipe@hotmail.com>
2022-11-16 16:53:22 -0500

    search and show the product

--- Code/cash_register/views.py
+++ Code/cash_register/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
+from .models import Product
 
 def index(request):
     context = {'latest_question_list': 1}
     return render(request, 'cash_register/index.html', context)
+
+def scan(request, code):
+    product = Product.objects.get(code=code)
+    context = {'product': product}
+    return render(request, 'cash_register/scan.html', context)
