d70fd8724e74c312cc1194be750c09beadf1209c
Jacky-Lee113 <jackychukhan.lee@ontariotechu.net>
2022-11-23 16:22:26 -0500

    Added screenshots and updated populate.py

--- Code/populate.py
+++ Code/populate.py
@@ -1,7 +1,7 @@
 from cash_register.models import Product
-p = Product(name: "Banana", code: 1111, price: 1.2)
+p = Product(name= "Coke", code= 1114, price= 1.5)
 p.save()
-p = Product(name="Apple", code=1112, price=0.89)
+p = Product(name="Oranges", code=1115, price= 0.62)
 p.save()
-p = Product(name="Milk", code=1113, price=3.4)
+p = Product(name="Cookies", code=1116, price= 2.2)
 p.save()
